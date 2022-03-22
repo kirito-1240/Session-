0e454dfbb7c5fc475d18b23b3d8ee475a9b1410a
Ananya <90745373+anunicn47@users.noreply.github.com>
2022-03-22 17:56:31 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -4,15 +4,7 @@ from pyrogram.types import InlineKeyboardButton
 class Data:
     # Start Message
     START = """
-Hey {}
 
-Welcome to {}
-
-If you don't trust this bot, 
-1) stop reading this message
-2) delete this chat
-
-Still reading?
 You can use me to generate pyrogram and telethon string session. Use below buttons to learn more !
 
 By @anu_music00bot
@@ -31,12 +23,12 @@ By @anu_music00bot
     # Rest Buttons
     buttons = [
         [InlineKeyboardButton("🔥 Start Generating Session 🔥", callback_data="generate")],
-        [InlineKeyboardButton("✨ Bot Status and More Bots ✨", url="https://t.me/StarkBots/7")],
+        [InlineKeyboardButton("✨ Bot Status and More Bots ✨", url="https://t.me/anu_pui")],
         [
             InlineKeyboardButton("How to Use ❔", callback_data="help"),
             InlineKeyboardButton("🎪 About 🎪", callback_data="about")
         ],
-        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/StarkBots")],
+        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/anu_pui")],
     ]
 
     # Help Message
@@ -57,11 +49,11 @@ By @anu_music00bot
 
 A telegram bot to generate pyrogram and telethon string session by @anu_music00bot
 
-Source Code : [Click Here](https://github.com/StarkBotsIndustries/StringSessionBot)
+
 
 Framework : [Pyrogram](docs.pyrogram.org)
 
 Language : [Python](www.python.org)
 
-Developer : @StarkProgrammer
+Developer : @an_unic_orn_47
     """
